48debd9bf2375b04102e386fd0d4c2ac3a862a2f
Carlton <carlton.hurdle@gmail.com>
2017-04-22 23:39:47 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var PORT = process.env.PORT || 8080;
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+//app list:
+app.use(bodyParser.json({ type: 'application/**json' }))
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+app.use(bodyParser.text({ type: 'text/html' }))
+app.listen(PORT, function() {
+	console.log("app listening on PORT: " + PORT);
+});
